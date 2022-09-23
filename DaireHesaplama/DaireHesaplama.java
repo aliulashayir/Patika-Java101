fdb5532e645008b8d22cce52ed63bde9907a621a
Ali Ulaş Hayır <aliulashayir@gmail.com>
2022-09-23 23:09:37 +0300

    DaireHesaplama

---
+++ DaireHesaplama/DaireHesaplama.java
@@ -0,0 +1,22 @@
+package DaireHesaplama;
+
+import java.util.Scanner;
+public class DaireHesaplama {
+
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        int r ,a;
+        double pi = 3.14;
+        System.out.println("yarı çapı girin :");
+        r = input.nextInt();
+
+        System.out.println("a açısını girin :");
+        a = input.nextInt();
+
+        double alan = (pi*(r*r)*a)/360;
+        double cevre = 2 * pi * r;
+
+        System.out.println("dairenin çevresi :"+cevre);
+        System.out.println("dairenin alanı :"+alan);
+    }
+}
